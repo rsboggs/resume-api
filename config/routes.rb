66616db272d5c8dbccfe1b42925eccd366c432bc
Robert Boggs <robertstephenboggs@gmail.com>
2016-05-08 17:06:37 -0700

    Set the routes constraints for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 ResumeApi::Application.routes.draw do
-
+  # API Definitions
+  namespace :api, defaults: { format: :json },
+    constraints: { subdomain: 'api' }, path: '/'  do
+    # We are going to list our resources here
+  end
 end
